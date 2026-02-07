4a56478d701cd015ebc884c0ffbd004eb25276a2
elvin morales <morales.elvin.16@gmail.com>
2026-02-07 13:35:11 -0600

    nabvar

--- inc/navbar.php
+++ inc/navbar.php
@@ -4,7 +4,7 @@
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
-      <a class="navbar-brand" href="#">inversiones</a>
+      <a class="navbar-brand" href="../index.php">inversiones</a>
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
           <a class="nav-link active" aria-current="page" href="../index.php">home</a>
