3f5c3fa16ec0e5bc8b1ceed68748c367ac4a1f9a
elvin morales <morales.elvin.16@gmail.com>
2026-02-15 17:03:50 -0600

    navbar

--- inc/navbar.php
+++ inc/navbar.php
@@ -1,19 +1,34 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
+<nav class="navbar navbar-expand-lg navbar-light bg-white shadow-sm">
   <div class="container-fluid">
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01">
+    <a class="navbar-brand fw-bold" href="/index.php">
+      <i class="fa-solid fa-seedling text-success me-2"></i>PROINO
+    </a>
+
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarMain">
       <span class="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
-      <a class="navbar-brand" href="/">inversiones</a>
-      
+
+    <div class="collapse navbar-collapse" id="navbarMain">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link active" href="/index.php">home</a>
+          <a class="nav-link" href="/index.php">
+            <i class="fa-solid fa-house-chimney me-1"></i> Home
+          </a>
         </li>
         <li class="nav-item">
-          <a class="nav-link active" href="/vistas/usuario.php">usuario</a>
+          <a class="nav-link" href="/vistas/usuario.php">
+            <i class="fa-solid fa-circle-user me-1"></i> Usuario
+          </a>
         </li>
       </ul>
+
+      <div class="d-flex align-items-center">
+        <div class="text-end me-3 d-none d-md-block">
+          <small class="text-muted d-block" style="font-size: 0.7rem; line-height: 1;">Hacienda</small>
+          <span class="fw-bold text-dark" style="font-size: 0.9rem;">San Francisco</span>
+        </div>
+        <img src="https://via.placeholder.com/40" alt="Logo Hacienda" class="rounded-circle border" style="width: 45px; height: 45px; object-fit: cover;">
+      </div>
     </div>
   </div>
 </nav>
