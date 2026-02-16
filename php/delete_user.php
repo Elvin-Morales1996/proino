973fa597363e14ccf28f63eea7a0eb9d5f44de12
Elvin-Morales1996 <elvinmorales.hernandez96@gmail.com>
2026-02-16 13:26:33 -0600

    delete

--- php/delete_user.php
+++ php/delete_user.php
@@ -1,6 +1,26 @@
 <?php
-echo '<h1>Delete User</h1>';
 
+include("../configuracion/config.php");
+$id = $_GET['id'];
+
+$usuario = conexion();
+$usuario = $usuario->prepare("DELETE FROM clientes WHERE id = :id");
+$usuario->execute([":id" => $id]);
+if ($usuario->rowCount()  ==1) {
+    echo "
+    <script>
+        alert('Usuario eliminado correctamente');
+        window.location.href = '../index.php';
+    </script>
+    ";
+} else {
+    echo "
+    <script>
+        alert('Error al eliminar el usuario');
+        window.location.href = '../index.php';
+    </script>";
+}           
+$usuario = null;
 
 
 ?>
